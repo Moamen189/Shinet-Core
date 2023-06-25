2fee642818c97cb7e4b181feb1eb543d1b59e55f
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-06-25 19:30:35 +0300

    Add project files.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+
+        public string GetProduct()
+        {
+            return "XXXXXXXX";
+        }
+    }
+}
