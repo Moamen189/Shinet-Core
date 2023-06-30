eb39a2b20b824adc75fab71709cccf46d456274a
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-06-30 09:34:14 +0300

    Apply Specification Pattern

---
+++ Core/Specification/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> criteria, List<Expression<Func<T, object>>> includes)
+        {
+            Criteria = criteria;
+            Includes = includes;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+    }
+}
