43217b552d19b178f64710d9aafb736bae7cc216
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-01 22:43:44 +0300

    REsolver Mapper

--- API/Helpers/MappingProfile.cs
+++ API/Helpers/MappingProfile.cs
@@ -10,7 +10,8 @@ namespace API.Helpers
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d=> d.ProductBrand , o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType , x => x.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType , x => x.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl ,  x  => x.MapFrom<ProductUrlResolver>());
         }
     }
 }
--- API/Helpers/ProductUrlResolver.cs
+++ API/Helpers/ProductUrlResolver.cs
@@ -1,6 +1,27 @@
-﻿namespace API.Helpers
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Helpers
 {
-    public class ProductUrlResolver
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
     {
+        private readonly IConfiguration configuration;
+
+        public ProductUrlResolver(IConfiguration Configuration)
+        {
+            configuration = Configuration;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+           if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return configuration["APIURL"] + source.PictureUrl;
+
+            }
+
+            return null;
+        }
     }
 }
