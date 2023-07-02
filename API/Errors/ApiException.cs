536d0095cedda2b8d54b1fe0ba4eea0241ad67ed
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-02 20:24:31 +0300

    Api Exception

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        public string Deatails { get; set; }
+        public ApiException(int statusCode, string message = null , string details = null) : base(statusCode, message)
+        {
+            Deatails= details;
+        }
+    }
+}
