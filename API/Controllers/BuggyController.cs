f52fba38b07b8120cd3b6accafeeba53be8b0bf7
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-02 19:19:22 +0300

    UpdateBuggyController

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,11 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Infastrucure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BuggyController : ControllerBase
+    public class BuggyController : BaseApiController
     {
+        private readonly StoreContext storeContext;
+
+        public BuggyController(StoreContext _storeContext)
+        {
+            storeContext = _storeContext;
+        }
+
+        [HttpGet("notfound")]
+
+        public IActionResult GetNotFoundRequest ()
+        {
+            var thing = storeContext.Products.Find(30);
+
+            if (thing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpGet("servererror")]
+
+        public IActionResult GetServerError()
+        {
+            var thing = storeContext.Products.Find(30);
+
+            var ThingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+
+        [HttpGet("badrequest")]
+
+        public IActionResult GetBadRequest()
+        {
+               return BadRequest();
+        }
+
+
+        [HttpGet("badrequest/{id}")]
+
+        public IActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
     }
 }
