c4b16df3244e39710402a31207b2308818cb4621
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-04 05:57:39 +0300

    Update

--- Client/Client/src/app/app.component.ts
+++ Client/Client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shinet Store';
+  constructor(private httpClient:HttpClient){}
+  products:any[] = [];
+  ngOnInit(): void {
+    this.httpClient.get("https://localhost:44398/api/Product?pageSize=50").subscribe({
+      next:(response:any) => this.products = response.data ,
+      error:error => console.log(error),
+      complete: ()=> {
+        console.log("Request Completed")
+        console.log("extra Staments")
+      }
+
+    })
+  }
 }
--- Client/Client/src/app/app.module.ts
+++ Client/Client/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
