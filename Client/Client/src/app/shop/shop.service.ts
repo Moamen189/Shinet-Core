85fe3492251a7027b90f9c8e68a3c13d5f98ef9c
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-07 19:56:30 +0300

    Update Services

--- Client/Client/src/app/shop/shop.service.ts
+++ Client/Client/src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/Paging';
 import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/Brands';
+import { Type } from '../shared/models/Types';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,14 @@ export class ShopService {
   baseUrl = "https://localhost:44398/api/"
   getProduct(){
     return this.http.get<Pagination<Product[]>>(this.baseUrl +'Product?pageSize=50')
+  }
+
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseUrl+'Product/brands')
+  }
+
+  getTypes(){
+    return this.http.get<Type[]>(this.baseUrl + 'Product/types')
   }
 
 
