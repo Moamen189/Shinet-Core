1b4ac31c5fff50831ed820a516a6f3091b99a720
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-07 16:05:58 +0300

    Update

--- Client/Client/src/app/app.module.ts
+++ Client/Client/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
