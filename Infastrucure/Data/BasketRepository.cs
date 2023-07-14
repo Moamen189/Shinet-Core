4471e4cc8ab2f7662cc02bdb5bab03645db8d3f9
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-14 22:35:55 +0300

    Identity Backages

--- Infastrucure/Data/BasketRepository.cs
+++ Infastrucure/Data/BasketRepository.cs
@@ -12,12 +12,12 @@ namespace Infastrucure.Data
 {
     public class BasketRepository : IBasketRepository
     {
-        private readonly IDatabase _database;
+         private readonly IDatabase _database;
         public BasketRepository(IConnectionMultiplexer redis)
         {
             _database = redis.GetDatabase();
         }
-
+       
         public async Task<bool> DeleteBasketAsync(string BasketId)
         {
             return await _database.KeyDeleteAsync(BasketId);
@@ -32,7 +32,7 @@ namespace Infastrucure.Data
 
         public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
         {
-            var created = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+            var created = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket),TimeSpan.FromDays(30));
             if (!created) return null;
             return await GetBasketAsync(basket.Id);
         }
