1765704d3ad18dacce2ad137ee9b5f2a6e305aaa
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-14 13:14:10 +0300

    UpdateBreadCrumb

--- Client/Client/src/app/core/core.module.ts
+++ Client/Client/src/app/core/core.module.ts
@@ -8,10 +8,12 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Toast, ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, ServerErrorComponent, NotFoundComponent],
+  declarations: [NavBarComponent, TestErrorComponent, ServerErrorComponent, NotFoundComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
@@ -19,8 +21,9 @@ import { Toast, ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
-    })
+    }),
+    BreadcrumbModule
   ],
-  exports:[NavBarComponent]
+  exports:[NavBarComponent , SectionHeaderComponent]
 })
 export class CoreModule { }
