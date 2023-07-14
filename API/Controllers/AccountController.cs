1da7fdbbed9c3268a557f8769e843b40e335af69
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-07-15 02:25:24 +0300

    TokenServices

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using API.Dtos;
+using API.Errors;
+using Core.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userManager , SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> login(LoginDto loginDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "Will Be Token",
+                DisplayName = user.DisplayName
+            };
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Regitser(RegisterDto registerDto)
+        {
+
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = "Token Will be here",
+                Email = user.Email
+            };
+        }
+    }
+}
