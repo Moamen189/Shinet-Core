73c04e3a97f5a2c6a67425bf68ca24b2d361a4a7
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-05 13:56:08 +0300

    Update ProductItemOrdered

--- Core/Entities/OrderAggregate/ProductItemOrdered.cs
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Core.Entities.OrderAggregate
 {
-    internal class ProductItemOrdered
+    public class ProductItemOrdered
     {
+        public ProductItemOrdered()
+        {
+
+        }
+
+        public ProductItemOrdered(int productitemId , string productName , string pictureUrl)
+        {
+            ProductItemId = productitemId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+
+        }
+        public int ProductItemId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+
     }
 }
