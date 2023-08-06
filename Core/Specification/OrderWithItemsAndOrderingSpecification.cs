8b0da2d0683e6771d35c9d9430d51b7e341d048a
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-06 15:49:52 +0300

    OrderWithItemsAndOrderingSpecification

--- Core/Specification/OrderWithItemsAndOrderingSpecification.cs
+++ Core/Specification/OrderWithItemsAndOrderingSpecification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.OrderAggregate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Core.Specification
 {
-    internal class OrderWithItemsAndOrderingSpecification
+    public class OrderWithItemsAndOrderingSpecification :BaseSpecification<Order>
     {
+
+        public OrderWithItemsAndOrderingSpecification(string email):base(o => o.BuyerEmail == email )
+        {
+
+            addIncludes(o => o.OrderItems);
+            addIncludes(o => o.DeliveryMethod);
+
+            AddOrderByDesending(o => o.OrderDate);
+
+
+        }
+
+        public OrderWithItemsAndOrderingSpecification(int id , string email):base(o => o.Id == id && o.BuyerEmail == email )
+        {
+
+            addIncludes(o => o.OrderItems);
+            addIncludes(o => o.DeliveryMethod);
+
+        }
     }
 }
