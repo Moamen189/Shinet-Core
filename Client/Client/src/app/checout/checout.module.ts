0f8c2e0e83368d23add991f3b803b40b3d1ef9af
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-06 22:48:57 +0300

    Checkout C

--- Client/Client/src/app/checout/checout.module.ts
+++ Client/Client/src/app/checout/checout.module.ts
@@ -4,11 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ChecoutRoutingModule } from './checout-routing.module';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { SharedModule } from '../shared/shared.module';
+import { CheckoutAddressComponent } from './checkout/checkout-address/checkout-address.component';
+import { CheckoutDeliveryComponent } from './checkout/checkout-delivery/checkout-delivery.component';
+import { CheckoutReviewComponent } from './checkout/checkout-review/checkout-review.component';
+import { CheckoutPaymentComponent } from './checkout/checkout-payment/checkout-payment.component';
+import { CheckoutSuccessComponent } from './checkout/checkout-success/checkout-success.component';
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CheckoutPaymentComponent,
+    CheckoutSuccessComponent
   ],
   imports: [
     CommonModule,
