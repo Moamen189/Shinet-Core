5768a782c73b247546bf64a47c4a5ddd8ca7409f
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-09 16:42:52 +0300

    OrderServices

--- Client/Client/src/app/orders/orders.service.ts
+++ Client/Client/src/app/orders/orders.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Order } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http:HttpClient) { }
+
+
+  getOrdersForUser()
+  {
+    return this.http.get<Order[]>(this.baseUrl+'Orders');
+  }
+
+  getOrderDetails(id:number)
+  {
+    return this.http.get<Order>(this.baseUrl+'Orders/'+id);
+  }
 }
