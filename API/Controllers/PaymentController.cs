431d0e28913521ff2b5196aed7d428cde6e7c787
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-10 14:54:59 +0300

    PaymentController

---
+++ API/Controllers/PaymentController.cs
@@ -0,0 +1,42 @@
+﻿using API.Errors;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : BaseApiController
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+
+
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+
+            var basket = await paymentService.CreateOrUpdatePaymentIntent(basketId);
+
+            if (basket == null)
+            {
+                return BadRequest(new ApiResponse(400, "Problem with your basket"));
+            }
+
+           return basket;
+
+
+        }
+
+    }
+}
