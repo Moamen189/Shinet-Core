847e4b41e5f9d0e517276a372df63bc1c63e5b1a
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-08-12 04:08:01 +0300

    Weebhook

--- API/Controllers/PaymentController.cs
+++ API/Controllers/PaymentController.cs
@@ -1,9 +1,12 @@
 ﻿using API.Errors;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Core.Interfaces;
+using Infastrucure.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace API.Controllers
 {
@@ -12,10 +15,13 @@ namespace API.Controllers
     public class PaymentController : BaseApiController
     {
         private readonly IPaymentService paymentService;
+        private readonly ILogger<PaymentController> _logger;
+        private const string Whsecret = "";
 
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(IPaymentService paymentService, ILogger<PaymentController> logger1)
         {
             this.paymentService = paymentService;
+            this._logger = logger1;
         }
 
 
@@ -34,6 +40,39 @@ namespace API.Controllers
             }
 
            return basket;
+
+
+        }
+
+
+        [HttpPost("webhook")]
+
+        public async Task<ActionResult> StripeWebHook()
+        {
+            var json = await new  StreamReader(Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], Whsecret);
+            PaymentIntent intent;
+
+            Core.Entities.OrderAggregate.Order order;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment succeded", intent.Id);
+                   
+                    break;
+
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment failed", intent.Id);
+      
+                    break;
+
+            }
+
+            return new EmptyResult();
 
 
         }
